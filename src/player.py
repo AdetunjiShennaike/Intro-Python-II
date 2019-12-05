35e73cd10758a414803f39caa2765f4d41a09064
Adetunji <Ashennaike@gmail.com>
2019-12-05 14:55:57 -0500

    made the room class and fixed player class a little bit

--- src/player.py
+++ src/player.py
@@ -1,9 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-  def __init__(self, name, species):
+  def __init__(self, name, species, currentRoom):
     self._name = name
     self._species = species
+    self._currentRoom = currentRoom
 
   def __str__(self):
-    return f"something something... my name was something... ahhh right its {self._name} and I am {self._species}"
+    return f"something something... my name was something... ahhh right its {self._name} and I am {self._species} in the {self._currentRoom}"
--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+  def __init__(self, name, items, desc):
+    self._name = name
+    self._items = items
+    self._desc = desc
+
+  def __str__(self):
+    return f"Room: {self._name}\nContained Items: {self._items}\nDescription of the Room: {self._desc}"
