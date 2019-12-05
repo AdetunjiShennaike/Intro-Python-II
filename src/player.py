5a83bc147d9524d6b1df4864eae1287cc07d428f
Adetunji <Ashennaike@gmail.com>
2019-12-05 14:36:31 -0500

    player created, will finish later with more functionality

--- src/player.py
+++ src/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+  def __init__(self, name, species):
+    self._name = name
+    self._species = species
+
+  def __str__(self):
+    return f"something something... my name was something... ahhh right its {self._name} and I am {self._species}"
